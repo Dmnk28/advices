00c812757ce5e5e328fa93214dc3882eaa2d47d2
Dominik Oesterle <dominik.oesterle@posteo.de>
2022-05-11 08:57:21 +0200

    advice.js finished

--- scripts/dist/advices.js
+++ scripts/dist/advices.js
@@ -1,2 +1,35 @@
 "use strict";
-const Test = 'Test';
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+const displayAdvice = () => __awaiter(void 0, void 0, void 0, function* () {
+    const adviceNumberSpan = document.getElementById('advice-number');
+    const adviceTextSpan = document.getElementById('advice-text');
+    const diceBtn = document.getElementById('advice-btn');
+    const getAdvice = fetch('https://api.adviceslip.com/advice')
+        .then(response => {
+        if (!response.ok)
+            throw new Error('Response was not OK.');
+        return response.json();
+    })
+        .then(({ slip }) => {
+        adviceNumberSpan.innerText = slip.id.toString();
+        adviceTextSpan.innerText = slip.advice;
+    })
+        .catch(error => {
+        throw new Error('Error in displayAdvice Function');
+    });
+});
+document.addEventListener('DOMContentLoaded', (event) => {
+    event.preventDefault();
+    displayAdvice();
+}, false);
+const handleClick = (event) => {
+    displayAdvice();
+};
--- scripts/src/advices.ts
+++ scripts/src/advices.ts
@@ -1,1 +1,37 @@
-const Test: string = 'Test';
+type Advice =  {
+    "slip": { 
+        "id": string, 
+        "advice": string,
+    }
+}
+
+const displayAdvice = async () => {
+    const adviceNumberSpan: HTMLSpanElement = document.getElementById('advice-number') as HTMLSpanElement;
+    const adviceTextSpan: HTMLSpanElement   = document.getElementById('advice-text') as HTMLSpanElement;
+    const diceBtn: HTMLAnchorElement = document.getElementById('advice-btn') as HTMLAnchorElement;
+
+
+    const getAdvice = fetch('https://api.adviceslip.com/advice')
+        .then(response => {
+            if (!response.ok) throw new Error('Response was not OK.')
+            return response.json();
+        })
+        .then(({ slip }) => {
+            adviceNumberSpan.innerText = slip.id.toString();
+            adviceTextSpan.innerText = slip.advice;
+        })
+        .catch(error => {
+            throw new Error('Error in displayAdvice Function');
+        })    
+
+}
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    event.preventDefault();
+    displayAdvice();
+}, false)
+
+
+const handleClick = (event: MouseEvent): void => {
+    displayAdvice();
+}
